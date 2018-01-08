1d2e39e42d186ac6f626d36488a20016bef360dd
Guest User <Guest@Epicodus-5A.local>
2018-01-08 11:54:59 -0800

    add java, handlebars

---
+++ IdeaProjects/Rectangle/src/main/java/App.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+import static spark.Spark.*;
+
+
+public class App {
+    public static void main(String[] args) {
+
+        get("/", (req, res) -> {
+
+            Rectangle rectangle = new Rectangle(3,2);
+            Rectangle otherRectangle = new Rectangle(12, 12);
+
+            Map<String, ArrayList<Rectangle>> model = new HashMap<>();
+            ArrayList myRectangleArrayList = Rectangle.getAll();
+            model.put("myRectangles", myRectangleArrayList);
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
